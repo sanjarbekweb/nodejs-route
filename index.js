80b7b8ea183aa01212d26767057a35b05f5e92f9
sanjarbekweb <sanjarbeks650@gmail.com>
2025-10-03 16:17:09 +0500

    add dotenv and cors middleware; update server port configuration

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express"
 import postRoute from "./routes/post.route.js"
 import userRoute from "./routes/user.route.js"
-
+import dotenv from 'dotenv'
+import cors from "cors"
+dotenv.config()
+const PORT = process.env.PORT
 const server = express()
 server.use(express.json())
+server.use(cors())
 
+server.get('/', (req,res)=>{
+	res.send({ok: true})
+})
 server.use('/posts', postRoute)
 server.use('/users', userRoute)
 
 
-server.listen(3000, ()=>{
+server.listen(PORT, ()=>{
 	console.info("server is running on 3000 port");
 })
